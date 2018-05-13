de1330f057b5ad9b8035c310aa914d695685e9d5
thug-lyfe <marco8jnv@hotmail.com>
2018-05-13 03:43:02 +0200

    redirects all files to smaller bites, for not using too much ram on just storage

--- reader_v2.js
+++ reader_v2.js
@@ -4,6 +4,7 @@ const cities = require("all-the-cities");
 const stringify = require('csv-stringify');
 const { fork } = require('child_process');
 const pool = require('fork-pool');
+var async = require('async');
 //const mongoID = require('mongodb').ObjectID;
 
 //let suc_title = 0; let suc_auth = 0; let suc_release = 0; let count = 0;
@@ -21,6 +22,11 @@ let psql_auth = []
 //let mongo_auth = []
 //let mongo_book = []
 //let mongo_city = [];
+
+
+
+
+
 fs.writeFile('csvs/mongo_auth.json', '', function () { console.log('done') })
 fs.writeFile('csvs/mongo_book.json', '', function () { console.log('done') })
 fs.writeFile('csvs/mongo_city.json', '', function () { console.log('done') })
@@ -29,13 +35,15 @@ fs.writeFile('csvs/psql_author.csv', '', function () { console.log('done') })
 fs.writeFile('csvs/psql_book.csv', '', function () { console.log('done') })
 fs.writeFile('csvs/psql_city.csv', '', function () { console.log('done') })
 fs.writeFile('csvs/psql_mention.csv', '', function () { console.log('done') })
+
+
 let city_ids = []
 
-let count = 0;
+let count = 37234;
 let count_fin = 0;
 let count_book = 0;
 let cp = new pool('./cityscan.js', null, null, {});
-let successFunc_meta = function (id, filename, auth, title, release, callback) {
+let successFunc_meta = async function (id, filename, auth, title, release, callback) {
     let auth_id = -1;
     let book_id = count_book++;
     for (var i = 0; i < psql_auth.length; i++) {
@@ -47,6 +55,7 @@ let successFunc_meta = function (id, filename, auth, title, release, callback) {
     if (auth_id == -1) {
         auth_id = psql_auth.length
         psql_auth.push([auth_id, auth])
+        appendToCsv([auth_id, "\"" + auth + "\""], "psql_author.csv")
         appendToJson({
             _id: "auth" + auth_id,
             name: auth
@@ -69,7 +78,7 @@ let failFunc = function (filename, str, id, dirname) {
     console.log(str, "filename: " + filename, "id: " + id, "dirname: " + dirname, "  ::  ", filename.substring(0, filename.indexOf(".")).match(/^[0-9]*$/))
 }
 
-let cpQ = function (content, book_id, mongo_temp) {
+let cpQ = async function (content, book_id, mongo_temp, callback) {
     let localTime = Date.now();
     cp.enqueue(content, (err, list) => {
         list.stdout.city_list.forEach(function (city_id) {
@@ -88,14 +97,15 @@ let cpQ = function (content, book_id, mongo_temp) {
         })
         appendToJson(mongo_temp, "mongo_book.json")
 
-        console.log("books fin: " + ++count_fin, "\tbooks remaining: " + (count - count_fin), "\t% done: " + Math.floor(count_fin * 10000 / count) / 100, "\ttotal time: "+(Date.now()-totalTime)/1000+"s","\tbook time: "+(Date.now()-localTime)/1000+"s")
+        console.log("books fin: " + ++count_fin, "\tbooks remaining: " + (count - count_fin), "\t% done: " + Math.floor(count_fin * 10000 / count) / 100, "\ttotal time: " + (Date.now() - totalTime) / 1000 + "s", )
 
         if (count_fin == count) {
             console.timeEnd("dbsave")
-            writeToCsv();
-            cp.drain((test) => {
-            });
+            //writeToCsv();
+            /*cp.drain((test) => {
+            });*/
         }
+        callback(book_id)
     })
 
 }
@@ -120,7 +130,7 @@ let appendToJson = function (content, file) {
         }
     });
 }
-
+/*
 let writeToCsv = function () {
     stringify(psql_auth, function (err, output) {
         fs.writeFile('csvs/psql_author.csv', output, 'utf8', function (err) {
@@ -131,9 +141,8 @@ let writeToCsv = function () {
             }
         });
     });
-}
-
-let somefunc = function (filename, dirname, content) {
+}*/
+let somefunc = async function (filename, dirname, content, callback) {
     let title = null; let auth = null; let release = null;
     let index = content.indexOf("[Etext #");
     let id = null;
@@ -156,7 +165,9 @@ let somefunc = function (filename, dirname, content) {
         release = "unknown";
         id = filename
         successFunc_meta(id, dirname.substring(6, dirname.length) + filename, auth, title, release, (book_id, mongo_temp) => {
-            cpQ(content, book_id, mongo_temp)
+            cpQ(content, book_id, mongo_temp, (res) => {
+                callback(res)
+            })
         });
 
     }
@@ -166,7 +177,9 @@ let somefunc = function (filename, dirname, content) {
         release = "1846";
         id = filename
         successFunc_meta(id, dirname.substring(6, dirname.length) + filename, auth, title, release, (book_id, mongo_temp) => {
-            cpQ(content, book_id, mongo_temp)
+            cpQ(content, book_id, mongo_temp, (res) => {
+                callback(res)
+            })
         });
     }
     else if (filename == "pntvw10.txt") {
@@ -175,7 +188,9 @@ let somefunc = function (filename, dirname, content) {
         release = "01-10-2001";
         id = filename
         successFunc_meta(id, dirname.substring(6, dirname.length) + filename, auth, title, release, (book_id, mongo_temp) => {
-            cpQ(content, book_id, mongo_temp)
+            cpQ(content, book_id, mongo_temp, (res) => {
+                callback(res)
+            })
         });
     }
     else if (filename == "Introduction_and_Copyright.txt") {
@@ -184,7 +199,9 @@ let somefunc = function (filename, dirname, content) {
         release = "14-03-1999";
         id = filename
         successFunc_meta(id, dirname.substring(6, dirname.length) + filename, auth, title, release, (book_id, mongo_temp) => {
-            cpQ(content, book_id, mongo_temp)
+            cpQ(content, book_id, mongo_temp, (res) => {
+                callback(res)
+            })
         });
     }
     else {
@@ -214,7 +231,9 @@ let somefunc = function (filename, dirname, content) {
 
             } else {
                 successFunc_meta(id, dirname.substring(6, dirname.length) + filename, auth, title, release, (book_id, mongo_temp) => {
-                    cpQ(content, book_id, mongo_temp)
+                    cpQ(content, book_id, mongo_temp, (res) => {
+                        callback(res)
+                    })
                 });
             }
         })
@@ -226,32 +245,32 @@ let someErr = function (err, index) {
     console.log(index, err)
 }
 
-function readFiles(dirname, onFileContent, onError, callback) {
-    fs.readdir(dirname, function (err, filenames) {
-        if (err) {
-            onError(err);
-            return;
-        }
-
-        filenames.forEach(function (filename) {
-            if (filename.indexOf(".") == -1) {
-                readFiles(dirname + filename + "/", somefunc, someErr);
+function readFiles(dirname) {
+    console.log(dirname + tmp_dirs[dir_count])
+    filenames = fs.readdirSync(dirname + tmp_dirs[dir_count] + "/");
+    filenames.forEach(function (file) {
+        fs.readFile(dirname + tmp_dirs[dir_count] + "/" + file, 'utf-8', function (err, content) {
+            
+            if (err) {
+                onError(err);
+                return;
             }
-            if (filename.indexOf(".txt") != -1) {
-                count++;
-                fs.readFile(dirname + filename, 'utf-8', function (err, content) {
-                    if (err) {
-                        onError(err);
-                        return;
-                    }
-                    onFileContent(filename, dirname, content);
-                });
-            }
-        });
-    });
+            somefunc(file, dirname + tmp_dirs[dir_count] + "/", content, (res) => {
+                if (++file_count == filenames.length) {
+                    dir_count++;
+                    file_count = 0;
+                    readFiles(dirname)
+                }
+            })
 
+        })
+    })
 }
+
+
+
 let scanCities_v2 = function (content, callback) {
+    
     let index = content.indexOf("*** START OF THIS PROJECT GUTENBERG") + 35;
     let end = content.indexOf("*** END OF THIS PROJECT GUTENBERG");
     if (end == -1) {
@@ -278,5 +297,54 @@ let scanCities_v2 = function (content, callback) {
 
 //use test/ for test files, or files/ for all files
 //remember test if at line 87.
-readFiles("files/", somefunc, someErr)
+//readFiles("files/", somefunc, 
 
+
+let redirect = function (target_dir, from_dir) {
+    fs.readdir(from_dir, function (err, filenames) {
+        if (err) {
+            throw err;
+            return;
+        }
+        filenames.forEach(function (filename) {
+            if (filename.indexOf(".") == -1) {
+                redirect(target_dir,from_dir + filename + "/");
+            }
+            if (filename.indexOf(".txt") != -1) {
+                if (count_files == 0) {
+                    if (!fs.existsSync(target_dir + count_dir)) {
+                        fs.mkdirSync(target_dir + count_dir)
+                    }
+                }
+                fs.rename(from_dir + filename, target_dir + count_dir + "/" + filename, (err) => {
+                    if (err) throw err;
+                    console.log('Rename complete!');
+                });
+
+                if (++count_files == 300) {
+                    count_files = 0
+                    count_dir++;
+                }
+            }
+        });
+    })
+}
+let redirect_v2 = function (target_dir, from_dir) {
+    if (!fs.existsSync(target_dir)) {
+        fs.mkdirSync(target_dir)
+    }
+    //redirect(target_dir, from_dir)
+    tmp_dirs= fs.readdirSync(target_dir);
+    readFiles(target_dir, somefunc)
+    
+}
+//for renaming
+let count_dir = 0;
+let count_files = 0;
+//for recursive
+let dir_count = 0;
+let file_count = 0;
+let target = 'files_final/'
+let tmp_dirs = null
+
+redirect_v2(target, 'files/')
